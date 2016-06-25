76a64e6532258da1f3da1b67d7493293af820871
bryan.jacobs <bryan.jacobs@martizcx.com>
2016-06-24 20:35:36 -0600

    add some basic logging to rest api

--- src/main/java/invest/rest/FundRest.java
+++ src/main/java/invest/rest/FundRest.java
@@ -2,6 +2,8 @@ package invest.rest;
 
 import invest.model.Fund;
 import invest.service.FundService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,12 +21,19 @@ import java.util.List;
 @RequestMapping("/fund")
 public class FundRest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FundRest.class);
+
     @Autowired
     FundService fundService;
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Fund> getWithPercentageChange() {
+
+        LOGGER.info("START: REST");
+
         List<Fund> funds = fundService.getFunds();
+
+        LOGGER.info("END: REST");
         return funds;
     }
 }
